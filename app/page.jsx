4df7573789416895b95c326fb1ab3870ad6f294c
sathwik_karne <karnesathwik@gmail.com>
2024-06-17 00:17:01 +0530

    Display Recent Properties in Home, custom 404 page and loading spinner

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,7 @@
 import Hero from '@/components/Hero'
+import HomeProperties from '@/components/HomeProperties'
 import InfoBoxes from '@/components/InfoBoxes'
+
 import React from 'react'
 
 // import Link from 'next/link'
@@ -9,6 +11,7 @@ const HomePage = () => {
     <div>
       <Hero/>
       <InfoBoxes/>
+      <HomeProperties/>
     </div>
   )
 }
