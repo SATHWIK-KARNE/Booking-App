2f14d0b4c8e163b4bb3448068dd5dcea6dfa3190
sathwik_karne <karnesathwik@gmail.com>
2024-06-26 02:15:31 +0530

    Bookmark functionality for a property

---
+++ components/BookmarkButton.jsx
@@ -0,0 +1,95 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { toast } from 'react-toastify';
+import { FaBookmark } from 'react-icons/fa';
+
+const BookmarkButton = ({ property }) => {
+  const { data: session } = useSession();
+  const userId = session?.user?.id;
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    //if we view /properties without signedIn still checkBookmarkStatus() is called and returns 401 error. So just to view /properties don't enter this useEffect
+    if (!userId) {
+      setLoading(false);
+      return;
+    }
+
+    // similar to handleClick() function to add bookmark. But we call /bookmarks/check to know isBookmarked 
+    const checkBookmarkStatus = async () => {
+      try {
+        const res = await fetch('/api/bookmarks/check', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            propertyId: property._id,
+          }),
+        });
+
+        if (res.status === 200) {
+          const data = await res.json();
+          setIsBookmarked(data.isBookmarked);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkBookmarkStatus();
+  }, [property._id, userId]);
+
+  const handleClick = async () => {
+    if (!userId) {
+      toast.error('You need to sign in to bookmark a property');
+      return;
+    }
+
+    try {
+      // post request to add a bookmark to this property
+      const res = await fetch('/api/bookmarks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          propertyId: property._id,
+        }),
+      });
+
+      if (res.status === 200) {
+        const data = await res.json();
+        toast.success(data.message);
+        setIsBookmarked(data.isBookmarked);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong');
+    }
+  };
+
+  if (loading) return <p className='text-center'>Loading...</p>;
+
+  return isBookmarked ? (
+    <button
+      onClick={handleClick}
+      className='bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'
+    >
+      <FaBookmark className='mr-2' /> Remove Bookmark
+    </button>
+  ) : (
+    <button
+      onClick={handleClick}
+      className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'
+    >
+      <FaBookmark className='mr-2' /> Bookmark Property
+    </button>
+  );
+};
+export default BookmarkButton;
